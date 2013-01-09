429416c4cc1bab5e8362afd2a98a06ffee141683
Jeff Higham <jeffhigham@gmail.com>
2013-01-09 14:47:29 -0700

    new tcx activities

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,5 @@
 class Activity < ActiveRecord::Base
-  attr_accessible :activityid, :creatorname, :productid, :sport, :unitid
+  attr_accessible :activityid, :creatorname, :productid, :sport, :unitid, :datafile
+  has_attached_file :datafile
+
 end
