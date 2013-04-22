e3a6ae0dc51a4ffff2109cbc14f693b222832ac4
Jeff Higham <jeffhigham@gmail.com>
2013-04-21 20:44:11 -0600

    Fixed login screen layout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
   skip_before_filter :check_login
+   layout "session"
 
   def new
   end
@@ -9,9 +10,11 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       if user.is_admin?
-        redirect_to users_path, :notice => "Logged in as an Admin!"
+        #redirect_to users_path, :notice => "Logged in as an Admin!"
+        redirect_to users_path
       else
-        redirect_to user_activities_path(user), :notice => "Logged in as a regular Joe!"
+        #redirect_to user_activities_path(user), :notice => "Logged in as a regular Joe!"
+        redirect_to user_activities_path(user)
       end
     else
       redirect_to login_path, :notice => "Invalid email or password"
