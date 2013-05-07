8b38f3de3f390c0cc3a69a47d1ee2c43fb657575
Jeff Higham <jeffhigham@gmail.com>
2013-05-07 11:02:07 -0600

    More work on thq.datafile.js

--- app/assets/javascripts/THQ/thq.activity.js
+++ app/assets/javascripts/THQ/thq.activity.js
@@ -2,7 +2,7 @@ ThqActivity = function(activity_id){
 
 	this.activity_id = activity_id;
 	this.name = "My " + activity_id;
-	this.get = new ThqDataFile();
-	this.get.loadRemote('/activities/'+ activity_id +'/load_trackpoint.json');
+	this.datafile = new ThqDataFile();
+	this.datafile.loadRemote('/activities/'+ activity_id +'/load_trackpoint.json');
 
 }
--- app/assets/javascripts/THQ/thq.datafile.js
+++ app/assets/javascripts/THQ/thq.datafile.js
@@ -1,62 +1,168 @@
 ThqDataFile = function(){
 
-	this.data_source = null;
-	this.request = null;
-	this.laps = 0;
-	this.context = "time" // 'time' or 'distance'
-	this.lap_context = "all" // 'all' or lap_index
-	this.scale_factor = 100; //constant to base data scailing % on.
-	this.laps_time = [];
-	this.laps_distance = [];
-	this.all_time = [];
-	this.all_distance = [];
-	this.wattage_data = []; //[{watts: nn, time:nn}, {watts:nn, time:nn}, {}];
-	this.wattage_laps = []; //[{watts: nn, time:nn}, {watts:nn, time:nn}, {}];
-	this.map_data = [[]]; //[{lng: nn, lat:nn}];
-	this.wattage = null;
-	this.current_dataset = [];
+	//var data_url = null; // url of JSON data
+	var ajax_request_obj = null; // ajax ajax_request_obj object.
+	var lap_count = 0; // number of lap_count in the activity.
+	var context = "time" // 'time' or 'distance'
+	var lap_context = "all" // 'all' or lap_index
+	var scale_factor = 99; //constant to base data scailing % on.
+	var lap_data_time = []; // lap array based on activity time (all data)
+	var lap_data_distance = []; // lap array based on distance (all data)
+	var all_data_time = []; // array of the whole activity based on time (all data)
+	var all_data_distance = []; // array of whole activity based on distance (all data)
+	var wattage_data = []; //[{watts: nn, time:nn}, {watts:nn, time:nn}, {}];
+	var wattage_laps = []; //[{watts: nn, time:nn}, {watts:nn, time:nn}, {}];
+	var map_lap_data = []; //[{lng: nn, lat:nn}];
+	var wattage_obj = null; // ThqWattage object created in loadRemote()
+	var current_dataset = []; // array of current dataset. This is updated often.
 
-	this.loadRemote = function(data_source) {
-		this.data_source = data_source;
-		this.request = $.ajax({
+	this.loadRemote = function(data_url) {
+			ajax_request_obj = $.ajax({
   		dataType: "text",
-  		url: this.data_source,
+  		url: data_url,
   		async: false
 		});
-		this.parse();
-		this.wattage = new ThqWattage(this.wattage_data);
+
+		this.parse_datafile();
+		wattage_obj = new ThqWattage(wattage_data);
 	}
 
-	this.trackpoints = function(){
-		return $.parseJSON(this.request.responseText.replace(';',''));
+	this.status = function(){
+		return {
+			context: context,
+			lap_context: lap_context,
+			scale_factor: scale_factor
+		};
 	}
 
-	this.wattage_at_lap = function(lap_id){
-		return new ThqWattage(this.wattage_laps[lap_id]);
-	}
+	// return currently active dataset for graph, map, etc.
+	this.dataset = function(selection){
 
-	this.scaled = function (scale_factor){
-		this.scale_factor = scale_factor;
-		var source_dataset = [];
-		var scaled_dataset = [];
-		console.info("Scaling at "+ scale_factor +"%, context: "+ this.context +"  lap.context: "+ this.lap_context );
+		// check for a request to change context.
+		if(typeof(selection) != "undefined"){
+			
+			// change context to "time".
+			if(selection == "time"){
+				context = "time";
+				// check if a lap is selected.
+				current_dataset = (lap_context == "all") ? all_data_time : lap_data_time[lap_context];
+			}
 
-		if( this.lap_context == "all"){
-			if(this.context == "time"){
-				source_dataset = this.all_time;
+			// change context to "distance"
+			else if(selection == "distance") {
+				context = "distance";
+				// check if a lap is selected
+				current_dataset = (lap_context == "all") ? all_data_distance : lap_data_distance[lap_context];
+			}
+
+			// an incorrect context selection. Warn and do nothing.
+			else{
+				console.warn("No changes to context. "+ selection +" is not valid.");
+			}
+
+		}
+
+		else { // selection == undefined
+
+			if( lap_context == "all"){
+				current_dataset = (context == "time") ? all_data_time : all_data_distance;
 			}
 			else {
-				source_dataset = this.all_distance;
+				current_dataset = (context == "time") ? lap_data_time[lap_context] : lap_data_distance[lap_context];
+			}			
+		}
+
+		//return current_dataset scaled appropriately with current scale_factor
+		return this.scaled();
+	}
+
+	this.lap = function(lap_id){
+
+		if( lap_id == lap_context){
+			console.info("No need to process lap_id:"+ lap_id +" == lap_context:"+ lap_context);
+		}
+		else {
+			// check for a lap reset.
+			if( lap_id == "all"){
+				lap_context = "all";
+			}
+			// check for a valid lap
+			else if( typeof(lap_data_time[lap_id]) != "undefined" ){
+				lap_context = lap_id;
+			}
+			// no changes, bad lap_id passed.
+			else {
+				console.warn("No changes made!! Bad lap_id:"+ lap_id +" lap_context: "+ lap_context);
+			}
+
+		}
+
+		return this.dataset();
+
+	}
+
+	// return wattage object for the whole activity.
+	this.wattage = function(){
+		return wattage_obj;
+	}
+
+	// create and return a wattage object base on a lap.
+	this.wattage_at_lap = function(lap_id){
+		return new ThqWattage(wattage_laps[lap_id]);
+	}
+
+	// polygon lines for map
+	this.map_data = function (lap_id){
+
+		// called without a lap id.
+		if(typeof(lap_id) == "undefined"){
+			console.info("Loading combined map data." );
+			// combine all laps into one.
+			var map_data = [];
+			for(var i=0; i<map_lap_data.length; i++){
+				map_data.push(map_lap_data[i]);
+			}
+			return map_data;
+		}
+		// return based on lap id.
+		else {
+			console.info("Loading map data for lap: "+ lap_id );
+			return [ map_lap_data[lap_id] ];
+		}
+	}
+
+	// return the raw trackpoints imported from the webserver ajax_request_obj.
+	// for the current activity.
+	this.trackpoints = function(){
+		return $.parseJSON(ajax_request_obj.responseText.replace(';',''));
+	}
+
+	this.scaled = function (percentage){
+		if(typeof(percentage) != "undefined"){
+			scale_factor = percentage;
+		}
+		var source_dataset = current_dataset; // set by dataset() and lap();
+		var scaled_dataset = [];
+		console.info("Scaling at "+ scale_factor +"%, context: "+ context +"  lap_context: "+ lap_context );
+
+		/*
+		if( lap_context == "all"){
+			if(context == "time"){
+				source_dataset = all_data_time;
+			}
+			else {
+				source_dataset = all_data_distance;
 			}
 		}
 		else{
-			if(this.context == "time"){
-				source_dataset = this.laps_time[this.lap_context];
+			if(context == "time"){
+				source_dataset = lap_data_time[lap_context];
 			}
 			else {
-				source_dataset = this.laps_distance[this.lap_context];
+				source_dataset = lap_data_distance[lap_context];
 			}
 		}
+		*/
 
 		var scale_now = Math.round( source_dataset.length / (source_dataset.length - (source_dataset.length*(scale_factor*.01))));
 		var loop_count = 0;	
@@ -77,7 +183,7 @@ ThqDataFile = function(){
 		return scaled_dataset;
 	}
 
-	this.parse = function(){ // this is a horrible mess.
+	this.parse_datafile = function(){ // this is a horrible mess.
 
 		/*
 			trackpoints[lap_id] = [{},{}, ...];
@@ -87,21 +193,22 @@ ThqDataFile = function(){
 		
 		var trackpoints = this.trackpoints();
 		var activity_time_offset = trackpoints[0][0]['time_seconds_epoch'];
-		var laps_time_offset = 0;
-		var laps_distance_offset = 0;
+		var lap_data_time_offset = 0;
+		var lap_data_distance_offset = 0;
 		var this_trackpoint = null;
 		var trackpoint_distance = 0;
 
 		for(var lap_id=0; lap_id<trackpoints.length; lap_id++){
 
 			// initialize lap array.
-			this.laps_time[lap_id] = [];
-			this.laps_distance[lap_id] = [];
-			this.wattage_laps[lap_id] = [];
+			lap_data_time[lap_id] = [];
+			lap_data_distance[lap_id] = [];
+			wattage_laps[lap_id] = [];
+			map_lap_data[lap_id] = [];
 			
 			// lap-based time and distance offsets.
-			laps_time_offset = trackpoints[lap_id][0]['time_seconds_epoch'];
-			laps_distance_offset = trackpoints[lap_id][0]['distance_feet'];
+			lap_data_time_offset = trackpoints[lap_id][0]['time_seconds_epoch'];
+			lap_data_distance_offset = trackpoints[lap_id][0]['distance_feet'];
 
 			// loop for trackpoints in current lap.
 			for(var trackpoint_id=0; trackpoint_id<trackpoints[lap_id].length; trackpoint_id++){
@@ -131,11 +238,11 @@ ThqDataFile = function(){
 				}
 				
 				// populate time-based data.
-				this.laps_time[lap_id].push([
+				lap_data_time[lap_id].push([
 					// time since activity started.
 					//this_trackpoint['time_seconds_epoch'] - activity_time_offset,
 					// time since lap started.
-					this_trackpoint['time_seconds_epoch'] - laps_time_offset,
+					this_trackpoint['time_seconds_epoch'] - lap_data_time_offset,
 					this_trackpoint['watts'],
 					this_trackpoint['heart_rate'],
 					this_trackpoint['cadence'],
@@ -147,11 +254,11 @@ ThqDataFile = function(){
 				]);
 
 				// whole activity
-				this.all_time.push([
+				all_data_time.push([
 					// time since activity started.
 					this_trackpoint['time_seconds_epoch'] - activity_time_offset,
 					// time since lap started.
-					//this_trackpoint['time_seconds_epoch'] - laps_time_offset,
+					//this_trackpoint['time_seconds_epoch'] - lap_data_time_offset,
 					this_trackpoint['watts'],
 					this_trackpoint['heart_rate'],
 					this_trackpoint['cadence'],
@@ -162,14 +269,14 @@ ThqDataFile = function(){
 					this_trackpoint['percent_grade']
 				]);
 
-				this.wattage_data.push(
+				wattage_data.push(
 					{
 						watts: this_trackpoint['watts'], 
 						time: this_trackpoint['time_seconds_epoch'] - last_trackpoint['time_seconds_epoch']
 					}
 				);
 
-				this.wattage_laps[lap_id].push(
+				wattage_laps[lap_id].push(
 					{
 						watts: this_trackpoint['watts'], 
 						time: this_trackpoint['time_seconds_epoch'] - last_trackpoint['time_seconds_epoch']
@@ -177,18 +284,18 @@ ThqDataFile = function(){
 				);
 
         if (this_trackpoint['lng'] > 0 || this_trackpoint['lat'] > 0) {
-            this.map_data[0].push( { lng: this_trackpoint['lng'], lat: this_trackpoint['lat'] } );
+            map_lap_data[lap_id].push( { lng: this_trackpoint['lng'], lat: this_trackpoint['lat'] } );
         }
  
 
 				// populate distance-bsed data.
 				if(trackpoint_distance > 0){
 						
-					this.laps_distance[lap_id].push([
+					lap_data_distance[lap_id].push([
 						// distance since activity started.
 						//this_trackpoint['distance_feet'],
 						// distance since lap started.
-						this_trackpoint['distance_feet'] - laps_distance_offset,
+						this_trackpoint['distance_feet'] - lap_data_distance_offset,
 						this_trackpoint['watts'],
 						this_trackpoint['heart_rate'],
 						this_trackpoint['cadence'],
@@ -199,11 +306,11 @@ ThqDataFile = function(){
 						this_trackpoint['percent_grade']
 					]);
 
-					this.all_distance.push([
+					all_data_distance.push([
 						// distance since activity started.
 						this_trackpoint['distance_feet'],
 						// distance since lap started.
-						//this_trackpoint['distance_feet'] - laps_distance_offset,
+						//this_trackpoint['distance_feet'] - lap_data_distance_offset,
 						this_trackpoint['watts'],
 						this_trackpoint['heart_rate'],
 						this_trackpoint['cadence'],
@@ -220,8 +327,11 @@ ThqDataFile = function(){
 
 		} // lap_id
 
-		this.laps = this.laps_time.length;
-		this.current_dataset = this.scaled(90);
+		lap_count = lap_data_time.length;
+		lap_context = "all";
+		context = "time";
+		current_dataset = all_data_time;
+		//current_dataset = this.scaled(90);
 
 	} // this.parse
 
